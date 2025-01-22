b0e94bd6b398417516bea8fae7459e4e04b70070
Ayo Tosan Samuel <46663234+Stosan@users.noreply.github.com>
2025-01-22 10:00:02 +0100

    ...

--- src/teams/resources/retrieve_vehicle_policy.py
+++ src/teams/resources/retrieve_vehicle_policy.py
@@ -27,16 +27,20 @@ class WebDriverPool:
         
     def _create_driver(self) -> webdriver.Chrome:
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--disable-infobars")
+        chrome_options.add_argument("disable-infobars")
         chrome_options.add_argument("window-size=1920,1080")
+        chrome_options.add_argument('--remote-debugging-pipe')
+        chrome_options.add_argument('--remote-debugging-port=9222')
         chrome_options.add_argument(
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.109 Safari/537.36"
         )
+        chrome_options.add_experimental_option('browserStartupTimeout', 1000000)
+
         return webdriver.Chrome(options=chrome_options)
     
     @contextmanager
--- src/workflow_orch/entry.py
+++ src/workflow_orch/entry.py
@@ -13,15 +13,15 @@ from src.teams.stirring_agent import super_graph
 # from src.parserssettlement_team_parser import extract_from_settlement_offer
 # from config.db_setup import SessionLocal
 # from src.teams.policy_review.agents import policy_review_graph
-# from src.teams.fraud_detection.agents import fraud_detection_graph
+from src.teams.fraud_detection.agents import fraud_detection_graph
 # from src.teams.settlement_offer.agents import settlement_offer_graph
 from langchain_core.messages import HumanMessage
 from src.utilities.helpers import _new_get_datetime
 
 init_vertexai()
-if __name__ == "__main__":
-    # delete_claim_report_by_id(SessionLocal(),90)
-    process_message(100)
+# if __name__ == "__main__":
+#     # delete_claim_report_by_id(SessionLocal(),90)
+#     process_message(100)
 
 
 # {
@@ -82,90 +82,90 @@ if __name__ == "__main__":
 # }
 
 
-# claim_data = {
-#     "nameOfInsured": "Olalekan Adisa",
-#     "policyNumber": "0000980404",
-#     "addressOfInsured": "53/55 Omotola street, Aguda Surulere",
-#     "phoneNumberOfInsured": "09016827203",
-#     "declaration": True,
-#     "extentOfLossOrDamage": [
-#         {
-#             "item": "Driver side mirror",
-#             "costPrice": 40000,
-#             "amountClaimed": 40000
-#         }
-#     ],
-#     "personInCharge": "Olalekan Adisa",
-#     "addressOfPersonInCharge": "53/55 Adetola Street Aguda Surulere",
-#     "permissionConfirmation": "Yes",
-#     "otherInsuranceConfirmation": "No",
-#     "purposeOfUse": "Personal use",
-#     "durationOfOwnership": "2 years",
-#     "incidentDetails": "The claimant reported that while driving to work, a reckless driver hit their car by the side, breaking the side mirror.",
-#     "previousOwner": "SKymit motors",
-#     "servicedBy": "A+ mechanics",
-#     "lastServiceDate": "2024-12-19",
-#     "totalMileage": "67000",
-#     "vehicleMake": "Hyundai",
-#     "claimType": "accident",
-#     "registrationNumber": "KSF 727 HB",
-#     "claimantName": "Olalekan Adisa",
-#     "vehicleColor": "Black",
-#     "typeOfBody": "Sonata",
-#     "yearOfManufacture": "2014",
-#     "chassisNumber": "5NPE34AF1FH095171",
-#     "engineNumber": "not provided",
-#     "vehicleLicenseNumber": "KSF 727 HB",
-#     "policeStationName": "not provided",
-#     "evidenceProvided": [
-#         "The image depicts significant damage to the driver's side exterior mirror of a vehicle.  The damage is extensive, showing the mirror's internal components completely exposed.  The outer casing of the mirror appears intact, but the internal mechanisms, including the wiring harness and indicator light assembly, are severely damaged and displaced from their normal positions.  The internal structure of the mirror housing is broken, indicating a forceful impact. The level of damage suggests the mirror is non-functional.  While the image quality is sufficient to ascertain the extent of the damage, the precise nature of the impact which caused the damage cannot be determined from the image alone.  The claimant should review the image and verify that the displayed damage accurately reflects the extent of their loss.\n - evidenceSourceUrl: https://storage.googleapis.com/masteryhive-insurance-claims/rawtest/Scenario1/20211001_164221.jpg",
-#         "- Invoice Information:\n  * Invoice Number: 0050\n  * Date: 30th Nov 2024\n  * Customer Name: Olalekan Adisa\n  * Customer Address: 53/55 Omotola street, Alora street, Lagos, Nigeria\n  * Vehicle Make/Model: Hyundai/Sonata\n  * Vehicle Registration Number: KSF 727 HB\n\n- Items and Cost:\n  * Driver side mirror: 1 x ₦40,000 = ₦40,000\n  * Labour Cost: 4hrs x ₦2,500 = ₦10,000\n\n- Total Cost:\n  * Total Amount Due: ₦50,000\n\n- Invoice Narration and Purpose: This invoice details the cost of repair for the driver side mirror of a Hyundai Sonata with registration number KSF 727 HB. The cost includes the price of a new driver side mirror and the labour cost for its repair.\n - evidenceSourceUrl: https://storage.googleapis.com/masteryhive-insurance-claims/rawtest/Scenario1/Auto_Shop_Invoice.pdf"
-#     ],
-#     "ssim": {
-#         "prelossUrl": "https://storage.googleapis.com/masteryhive-insurance-claims/rawtest/preloss/preloss_0000980404.jpg",
-#         "claimUrl": "https://storage.googleapis.com/masteryhive-insurance-claims/rawtest/Scenario1/20211001_164221.jpg"
-#     },
-#     "dateClaimFiled": "December 15 2024",
-#     "repairInvoice": "- Invoice Information:\n  * Invoice Number: 0050\n  * Date: 30th Nov 2024\n  * Customer Name: Olalekan Adisa\n  * Customer Address: 53/55 Omotola street, Alora street, Lagos, Nigeria\n  * Vehicle Make/Model: Hyundai/Sonata\n  * Vehicle Registration Number: KSF 727 HB\n\n- Items and Cost:\n  * Driver side mirror: 1 x ₦40,000 = ₦40,000\n  * Labour Cost: 4hrs x ₦2,500 = ₦10,000\n\n- Total Cost:\n  * Total Amount Due: ₦50,000\n\n- Invoice Narration and Purpose: This invoice details the cost of repair for the driver side mirror of a Hyundai Sonata with registration number KSF 727 HB. The cost includes the price of a new driver side mirror and the labour cost for its repair.\n"
-# }
+claim_data = {
+    "nameOfInsured": "Olalekan Adisa",
+    "policyNumber": "0000980404",
+    "addressOfInsured": "53/55 Omotola street, Aguda Surulere",
+    "phoneNumberOfInsured": "09016827203",
+    "declaration": True,
+    "extentOfLossOrDamage": [
+        {
+            "item": "Driver side mirror",
+            "costPrice": 40000,
+            "amountClaimed": 40000
+        }
+    ],
+    "personInCharge": "Olalekan Adisa",
+    "addressOfPersonInCharge": "53/55 Adetola Street Aguda Surulere",
+    "permissionConfirmation": "Yes",
+    "otherInsuranceConfirmation": "No",
+    "purposeOfUse": "Personal use",
+    "durationOfOwnership": "2 years",
+    "incidentDetails": "The claimant reported that while driving to work, a reckless driver hit their car by the side, breaking the side mirror.",
+    "previousOwner": "SKymit motors",
+    "servicedBy": "A+ mechanics",
+    "lastServiceDate": "2024-12-19",
+    "totalMileage": "67000",
+    "vehicleMake": "Hyundai",
+    "claimType": "accident",
+    "registrationNumber": "KSF 727 HB",
+    "claimantName": "Olalekan Adisa",
+    "vehicleColor": "Black",
+    "typeOfBody": "Sonata",
+    "yearOfManufacture": "2014",
+    "chassisNumber": "5NPE34AF1FH095171",
+    "engineNumber": "not provided",
+    "vehicleLicenseNumber": "KSF 727 HB",
+    "policeStationName": "not provided",
+    "evidenceProvided": [
+        "The image depicts significant damage to the driver's side exterior mirror of a vehicle.  The damage is extensive, showing the mirror's internal components completely exposed.  The outer casing of the mirror appears intact, but the internal mechanisms, including the wiring harness and indicator light assembly, are severely damaged and displaced from their normal positions.  The internal structure of the mirror housing is broken, indicating a forceful impact. The level of damage suggests the mirror is non-functional.  While the image quality is sufficient to ascertain the extent of the damage, the precise nature of the impact which caused the damage cannot be determined from the image alone.  The claimant should review the image and verify that the displayed damage accurately reflects the extent of their loss.\n - evidenceSourceUrl: https://storage.googleapis.com/masteryhive-insurance-claims/rawtest/Scenario1/20211001_164221.jpg",
+        "- Invoice Information:\n  * Invoice Number: 0050\n  * Date: 30th Nov 2024\n  * Customer Name: Olalekan Adisa\n  * Customer Address: 53/55 Omotola street, Alora street, Lagos, Nigeria\n  * Vehicle Make/Model: Hyundai/Sonata\n  * Vehicle Registration Number: KSF 727 HB\n\n- Items and Cost:\n  * Driver side mirror: 1 x ₦40,000 = ₦40,000\n  * Labour Cost: 4hrs x ₦2,500 = ₦10,000\n\n- Total Cost:\n  * Total Amount Due: ₦50,000\n\n- Invoice Narration and Purpose: This invoice details the cost of repair for the driver side mirror of a Hyundai Sonata with registration number KSF 727 HB. The cost includes the price of a new driver side mirror and the labour cost for its repair.\n - evidenceSourceUrl: https://storage.googleapis.com/masteryhive-insurance-claims/rawtest/Scenario1/Auto_Shop_Invoice.pdf"
+    ],
+    "ssim": {
+        "prelossUrl": "https://storage.googleapis.com/masteryhive-insurance-claims/rawtest/preloss/preloss_0000980404.jpg",
+        "claimUrl": "https://storage.googleapis.com/masteryhive-insurance-claims/rawtest/Scenario1/20211001_164221.jpg"
+    },
+    "dateClaimFiled": "December 15 2024",
+    "repairInvoice": "- Invoice Information:\n  * Invoice Number: 0050\n  * Date: 30th Nov 2024\n  * Customer Name: Olalekan Adisa\n  * Customer Address: 53/55 Omotola street, Alora street, Lagos, Nigeria\n  * Vehicle Make/Model: Hyundai/Sonata\n  * Vehicle Registration Number: KSF 727 HB\n\n- Items and Cost:\n  * Driver side mirror: 1 x ₦40,000 = ₦40,000\n  * Labour Cost: 4hrs x ₦2,500 = ₦10,000\n\n- Total Cost:\n  * Total Amount Due: ₦50,000\n\n- Invoice Narration and Purpose: This invoice details the cost of repair for the driver side mirror of a Hyundai Sonata with registration number KSF 727 HB. The cost includes the price of a new driver side mirror and the labour cost for its repair.\n"
+}
 
 
-# if __name__ == "__main__":
-#     # delete_claim_report_by_id(SessionLocal(),91)
-#     claim_data = get_claim_from_database("96")
-#     # print(claim_data)
-#     print()
-#     claim_data['dateClaimFiled'] = _new_get_datetime(claim_data["createdAt"])
-#     if len(claim_data['resourceUrls']) != 0:
-#         # print(claim_data['resourceUrls'])
-#         print()
-#         result = asyncio.run(classify_supporting_documents(claim_data))
-#         claim_data = result
-#     if claim_data["claimType"] in ["Accident","accident"]:
-#         claim_data = AccidentClaimData(**claim_data)
-#     else:
-#         claim_data = TheftClaimData(**claim_data)
+if __name__ == "__main__":
+    # delete_claim_report_by_id(SessionLocal(),91)
+    claim_data = get_claim_from_database(100)
+    # print(claim_data)
+    print()
+    claim_data['dateClaimFiled'] = _new_get_datetime(claim_data["createdAt"])
+    if len(claim_data['resourceUrls']) != 0:
+        # print(claim_data['resourceUrls'])
+        print()
+        result = asyncio.run(classify_supporting_documents(claim_data))
+        claim_data = result
+    if claim_data["claimType"] in ["Accident","accident"]:
+        claim_data = AccidentClaimData(**claim_data)
+    else:
+        claim_data = TheftClaimData(**claim_data)
 
-#     # print(claim_data)
-#     for s in super_graph.stream(
-#         {"messages": [HumanMessage(content=(
-#                             "Important information:"
-#                             "\nThis service is currently run in Nigeria, this means:"
-#                             "\n1. The currency is \u20a6"
-#                             f"\n\nBegin this claim processing using this claim form JSON data:\n {claim_data.model_dump()}"
-#                             "\n\nYOU MUST USE THE SUMMARY TEAM TO PRESENT THE RESULT OF THIS TASK."
-#                         ))],
-#          "claim_form_json":[HumanMessage(
-#                         content=json.dumps(claim_data.model_dump()))]
-#                         }
-#     ):
-#         if "__end__" not in s:
-#             # Extract content values from the dictionary
-#             for key, value in s.items():
-#                 if isinstance(value, dict) and "agent_history" in value:
-#                     for message in value["agent_history"]:
-#                         print(message.content)
-#                 elif isinstance(value, dict) and "messages" in value:
-#                     for message in value["messages"]:
-#                         print(message.content)
-#                         # print(extract_from_fraud_checks(message.content))
+    # print(claim_data)
+    for s in fraud_detection_graph.stream(
+        {"messages": [HumanMessage(content=(
+                            "Important information:"
+                            "\nThis service is currently run in Nigeria, this means:"
+                            "\n1. The currency is \u20a6"
+                            f"\n\nBegin this claim processing using this claim form JSON data:\n {claim_data.model_dump()}"
+                            "\n\nYOU MUST USE THE SUMMARY TEAM TO PRESENT THE RESULT OF THIS TASK."
+                        ))],
+         "claim_form_json":[HumanMessage(
+                        content=json.dumps(claim_data.model_dump()))]
+                        }
+    ):
+        if "__end__" not in s:
+            # Extract content values from the dictionary
+            for key, value in s.items():
+                if isinstance(value, dict) and "agent_history" in value:
+                    for message in value["agent_history"]:
+                        print(message.content)
+                elif isinstance(value, dict) and "messages" in value:
+                    for message in value["messages"]:
+                        print(message.content)
+                        # print(extract_from_fraud_checks(message.content))
